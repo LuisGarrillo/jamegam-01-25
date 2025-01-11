9524f152c8b5169b332244d430c709450a540863
Mel <garrillofeeling@gmail.com>
2025-01-11 18:07:07 -0400

    Implement pause on ui display

--- scripts/ui/ui_display.gd
+++ scripts/ui/ui_display.gd
@@ -3,6 +3,10 @@ class_name UiDisplay
 
 @onready var title: Control = $Title
 @onready var transition_holder: AnimatedSprite2D = $TransitionHolder
+@onready var tutorial: Tutorial = $Tutorial
+
+const PAUSE = preload("res://scenes/ui/pause.tscn")
+var pause
 
 var current_step : String
 var to
@@ -17,23 +21,35 @@ var destination: Dictionary = {
 }
 signal transition_finished
 
+
+func _ready():
+	pause = PAUSE.instantiate()
+
+
 func start_transition(new_to) -> void:
 	to = new_to
 	current_step = "in"
 	transition(current_step)
 
+
 func transition(step: String) -> void:
 	transition_holder.play(step)
+
 
 func finish_transition() -> void:
 	current_step = "out"
 	transition(current_step)
 
+
 func unload_title() -> void:
 	remove_child(title)
 
+
 func load_title() -> void:
 	add_child(title)
+
+func toggle_pause() -> void:
+	add_child(pause) if not pause in get_children() else remove_child(pause)
 
 func _on_transition_holder_animation_finished() -> void:
 	if current_step == "in":
